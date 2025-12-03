49fd6c5c75e98bb42bcc710f94060402ed6f3f9f
Javlonbek Xoliqulov <javlonbekh2007@gmail.com>
2025-12-03 17:40:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4719990817-ac928",
+  "appId": "1:920717575854:web:1785691cdca8722b774f3f",
+  "apiKey": "AIzaSyBJotR_66GNfD79XIptFuEP40Jvjn5xvBw",
+  "authDomain": "studio-4719990817-ac928.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "920717575854"
+};
